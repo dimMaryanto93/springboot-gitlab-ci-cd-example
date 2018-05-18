e43b8aedeeb0eb810d1552114036074ba9cac567
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2018-05-18 15:06:42 +0700

    init project

---
+++ src/main/java/com/maryanto/dimas/example/gitlabciexample/GitlabCiExampleApplication.java
@@ -0,0 +1,12 @@
+package com.maryanto.dimas.example.gitlabciexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabCiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlabCiExampleApplication.class, args);
+	}
+}
